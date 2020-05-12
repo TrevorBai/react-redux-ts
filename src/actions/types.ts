5f4745717ae61825a161a9a53ebd5aa0a138ee73
Trevor Bai <baijin2011@gmail.com>
2020-05-11 22:56:22 -0400

    first commit

---
+++ src/actions/types.ts
@@ -0,0 +1,7 @@
+export enum ActionTypes {
+  // By default
+  // fetchTodos = 0
+  // something = 1
+  // somethingElse = 2
+  fetchTodos
+}
---
+++ src/components/App.tsx
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Todo, fetchTodos } from '../actions';
+import { StoreState } from '../reducers';
+
+interface AppProps {
+  todos: Todo[];
+  fetchTodos(): any;
+}
+
+class _App extends Component<AppProps> {
+  // componentDidMount() {
+  //   this.props.fetchTodos();
+  // }
+  onButtonClick = (): void => {
+    this.props.fetchTodos();
+  };
+
+  renderList(): JSX.Element[] {
+    return this.props.todos.map((todo: Todo) => {
+      return <div key={todo.id}>{todo.title}</div>;
+    });
+  }
+
+  render() {
+    // console.log(this.props.todos);
+    return (
+      <div>
+        <button onClick={this.onButtonClick}>Fetch</button>
+        {this.renderList()}
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = ({ todos }: StoreState): { todos: Todo[] } => {
+  return {
+    todos,
+  };
+};
+
+// export connected version of _APP, i.e. APP
+export const App = connect(mapStateToProps, {
+  fetchTodos,
+})(_App);
